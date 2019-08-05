1020175e84a14cd8cba4706397dbdfb8f826f324
Luís Costa <lmbcosta@hotmail.com>
2019-08-05 21:14:55 +0100

    Initial Commit

---
+++ TransitionAnimations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TransitionAnimations
+//
+//  Created by Luis  Costa on 05/08/2019.
+//  Copyright © 2019 Luis  Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
