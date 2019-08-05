866eeec41739de03b8c64b1ce05ff76b10604d01
Luís Costa <lmbcosta@hotmail.com>
2019-08-05 22:37:33 +0100

    ListCell added to project

---
+++ TransitionAnimations/Views/DetailCell/ListCell.swift
@@ -0,0 +1,17 @@
+//
+//  ListCell.swift
+//  TransitionAnimations
+//
+//  Created by Luis  Costa on 05/08/2019.
+//  Copyright © 2019 Luis  Costa. All rights reserved.
+//
+
+import UIKit
+
+class ListCell: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
